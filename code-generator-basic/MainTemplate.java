a03bb715d194bbf88a3e8b5f5cdd0b8bd06449d0
seedoilz <seedoilz609@qq.com>
2024-02-24 22:12:41 +0800

    first commit

---
+++ code-generator-basic/MainTemplate.java
@@ -0,0 +1,27 @@
+package com.seedoilz.model;
+
+import java.util.Scanner;
+
+/**
+ * ACM 输入模版（多数之和）
+ * @author seedoilz
+ */
+public class MainTemplate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            int n = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+
+            int sum = 0;
+            for (int num : arr) {
+                sum += num;
+            }
+
+            System.out.println("求和结果： + sum);
+        scanner.close();
+    }
+}
